337ddcd0915810fb053b84ec8b7a3a2eb89a5ef5
reddyakanksha98 <66587566+reddyakanksha98@users.noreply.github.com>
2022-05-29 13:05:24 +0530

    add dice game

---
+++ index.js
@@ -0,0 +1,22 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomImage1Source = "images/dice"+randomNumber1+".png";
+
+document.querySelector(".dice .img1").setAttribute("src", randomImage1Source);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImage2Source = "images/dice"+randomNumber2+".png";
+
+document.querySelector(".dice .img2").setAttribute("src", randomImage2Source);
+
+
+if(randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw";
+}
+else if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!! 🚩";
+}
+else if(randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!! 🚩";
+}
